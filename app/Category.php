d7b3eda3eb71b7a032b7203c19fd2d68806a6453
VienLe93 <lecongvien.dct@gmail.com>
2017-06-30 18:34:01 +0700

    update CategoryModel

--- app/Category.php
+++ app/Category.php
@@ -6,14 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-      protected $table = 'categories';
-      protected $fillable = [
-            'name',
-            'slug',
-            'status'
-      ];
-      public function products()
-      {
-         return $this->hasMany('App\Product');
-      }
+    protected $table = 'categories';
+    protected $fillable = [
+        'name',
+        'slug',
+        'status'
+    ];
+
+    public function product()
+    {
+        return $this->hasOne('App\Product');
+    }
 }
