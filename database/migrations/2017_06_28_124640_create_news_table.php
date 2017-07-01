b1b0172a7c533f30c977378edf4c60e959555d08
LuongVanTien <luongvantiendt13ctt01@gmail.com>
2017-07-01 12:31:09 +0700

    update news table migration

--- database/migrations/2017_06_28_124640_create_news_table.php
+++ database/migrations/2017_06_28_124640_create_news_table.php
@@ -17,11 +17,14 @@ class CreateNewsTable extends Migration
             $table->increments('id');
             $table->integer('list_new_id')->unsigned()->nullable();
             $table->foreign('list_new_id')->references('id')->on('list_news');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('title', 255);
             $table->string('slug', 255);
             $table->string('description', 255);
+            $table->integer('view');
             $table->string('image', 255);
-            $table->string('content', 255);
+            $table->mediumText('content');
             $table->timestamps();
         });
     }
