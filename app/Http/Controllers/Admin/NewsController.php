cf7eb5c2eddd6d6a50c717908684c82e54144111
LuongVanTien <luongvantiendt13ctt01@gmail.com>
2017-07-03 18:57:36 +0700

    news admin

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\ListNew;
+use App\NewsComment;
+use App\News;
+use App\Tag;
+
+class NewsController extends Controller
+{
+    //
+    public function index()
+    {
+        $news = News::all();
+        return view('admin.news.index')->with('news', $news);
+    }
+    
+    public function create()
+    {
+        $listNews = ListNew::pluck('name', 'id');
+        $tags = Tag::pluck('name', 'id');
+        return view('admin.news.create')->with('listNews', $listNews)->with('tags', $tags);
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+                'title'=>'required|unique:News,title',
+                'list_new_id'=>'required',
+                'description'=>'required',
+                'content'=>'required',
+                'image'=>'required|image|mimes:jpg,png,jpeg',
+            ], [
+                'title.required'=>'Bạn chưa nhập tiêu đề tin',
+                'title.unique'=>'Tiêu đề tin đã tồn tại',
+                'list_new_id.required'=>'Bạn chưa chọn danh mục',
+                'description.required'=>'Bạn chưa nhập mô tả',
+                'content.required'=>'Bạn chưa nhập nội dung',
+                'image.required'=>'Bạn chọn ảnh đại diện cho tin',
+                'image.mimes'=>'Ảnh đại diện phải là tệp có đuôi jpg, png.',
+                'image.image'=>'Tệp bạn chọn không phải là hình ảnh',
+            ]);
+        $news = new News;
+        $news->title = $request->title;
+        $news->slug = str_slug($request->title, "-");
+        $news->list_new_id = $request->list_new_id;
+        $news->description = $request->description;
+        $news->content = $request->content;
+        $news->view = 0;
+        $session_id = \Auth::user()->id;
+        $news->user_id = $session_id;
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $name = $file->getClientOriginalName();
+            $image = time()."_".$name;
+            $file->move("uploads/news", $image);
+            $news->image = $image;
+        }
+        $news->save();
+        $news->tags()->sync($request->tags, false);
+
+        return redirect('admin/news/create')->withSuccess('Thêm tin thành công');
+    }
+
+    public function edit($id)
+    {
+        $news = News::find($id);
+        $listNews = ListNew::pluck('name', 'id');
+        $tags = Tag::pluck('name', 'id');
+        return view('admin.news.edit')->with('news', $news)->with('listNews', $listNews)->with('tags', $tags);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+                'title'=>'required',
+                'list_new_id'=>'required',
+                'description'=>'required',
+                'content'=>'required',
+                'image'=>'image|mimes:jpg,png,jpeg',
+            ], [
+                'title.required'=>'Bạn chưa nhập tiêu đề tin',
+                'list_new_id.required'=>'Bạn chưa chọn danh mục',
+                'description.required'=>'Bạn chưa nhập mô tả',
+                'content.required'=>'Bạn chưa nhập nội dung',
+                'image.mimes'=>'Ảnh đại diện phải là tệp có đuôi jpg, png.',
+                'image.image'=>'Tệp bạn chọn không phải là hình ảnh',
+
+            ]);
+        $news = News::find($id);
+        $news->title = $request->title;
+        $news->slug = str_slug($request->title, "-");
+        $news->list_new_id = $request->list_new_id;
+        $news->description = $request->description;
+        $news->content = $request->content;
+        $session_id = \Auth::user()->id;
+        $news->user_id = $session_id;
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $name = $file->getClientOriginalName();
+            $image = time()."_".$name;
+            $file->move("uploads/news", $image);
+            if (file_exists("uploads/news/".$news->image)) {
+                //unlink("uploads/news/".$news->image);
+            }
+            $news->image = $image;
+        }
+        $news->save();
+        if (isset($request->tags)) {
+            $news->tags()->sync($request->tags);
+        } else {
+            $news->tags()->sync(array());
+        }
+        
+        return redirect('admin/news/edit/'.$id)->withSuccess('Cập nhật tin thành công');
+    }
+    public function destroy($id)
+    {
+        $news = News::find($id);
+        $news->delete();
+        return redirect('admin/news')->withSuccess("Xóa tin thành công");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,19 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('/admin', function () {
     return view('demo');
 });
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
+    Route::get('news', 'NewsController@index');
+    Route::get('news/create', 'NewsController@create');
+    Route::post('news', 'NewsController@store');
+    Route::get('news/edit/{id}', 'NewsController@edit');
+    Route::post('news/edit/{id}', 'NewsController@update');
+    Route::get('news/delete/{id}', 'NewsController@destroy');
+
+    Route::get('tags', 'TagController@index');
+    Route::get('tags/create', 'TagController@create');
+    Route::post('tags', 'TagController@store');
+    Route::get('tags/edit/{id}', 'TagController@edit');
+    Route::post('tags/edit/{id}', 'TagController@update');
+    Route::get('tags/delete/{id}', 'TagController@destroy');
+});
