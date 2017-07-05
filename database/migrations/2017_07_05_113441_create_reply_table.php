ac6d9953b83d04b488c798089d1bdfa0e82d4c0a
LuongVanTien <luongvantiendt13ctt01@gmail.com>
2017-07-05 18:36:36 +0700

    add migration create_reply_table

---
+++ database/migrations/2017_07_05_113441_create_reply_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReplyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reply', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('news_comment_id')->unsigned()->nullable();
+            $table->foreign('news_comment_id')->references('id')->on('news_comments');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reply');
+    }
+}
