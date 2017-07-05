bf49ec6a7c7631c5981d93d870fda03cd2ef5aab
ShinjiQ <quangdhqnam@gmail.com>
2017-07-05 19:36:13 +0700

    page-static

--- resources/views/warrantys/warranty.blade.php
+++ resources/views/warrantys/warranty.blade.php
@@ -1,31 +1,31 @@
 @extends('layouts.master')
 @section('content')
-	<h3>TRUNG TÂM BẢO HÀNH TRÊN TOÀN QUỐC</h3>
-	<link rel="stylesheet" href="../public/css/footer.css">
-	<link rel="stylesheet" href="../public/css/warranty.css">
-	<p><b>Thegioididong.com xin lỗi vì sự cố khiến điện thoại của quý khách bị hỏng và phải đi bảo hành. Thế Giới Di Động có 2 hỗ trợ dành riêng cho khách hàng mua điện thoại tại TGDĐ trong thời gian đi bảo hành như sau:
-	1. Nếu quý khách hàng muốn mượn máy để sử dụng trong thời gian gửi máy đi bảo hành, quý khách có thể mượn máy tại quầy máy đã sử dụng. Chi tiết giá trị máy mượn quý khách có thể hỏi nhân viên mượn máy hoặc xem trên biên nhận máy bảo hành.
-	2. Nếu máy gửi đi bảo hành quá 15 ngày (30 ngày đối với sản phẩm của Apple) hãng bảo hành chưa trả máy cho khách hàng, TGDĐ có thể hỗ trợ khách thu lại máy hỏng với mức giá hỗ trợ (đối với các sản phẩm iPhone sẽ áp dụng theo chính sách của Apple).
-	Lưu ý: Chỉ áp dụng thu mua cho điện thoại và phải còn trong điều kiện bảo hành.</b></p>
-	<p> <b>Chọn hãng</b> bảo hành: </p>
-	<div class="logo">
-		<ul style="list-style: none; width: 100%;padding: 10px 0;">
-			<li class="logo"><a href=""> <img class="thumbnail" src="../public/img/apple.png" alt=""></a></li>
-			<li class="logo"><a href=""> <img class="thumbnail" src="../public/img/Sony.png" alt=""></a></li>
-			<li class="logo"><a href=""> <img class="thumbnail" src="../public/img/Samsung.png" alt=""></a></li>
-			<li class="logo"><a href=""> <img class="thumbnail" src="../public/img/LG.png" alt=""></a></li>
-			<li class="logo"><a href=""> <img class="thumbnail" src="../public/img/nokia.png" alt=""></a></li>
-			<li class="logo"><a href=""> <img class="thumbnail" src="../public/img/oppo.png" alt=""></a></li>
-			<li class="logo"><a href=""> <img class="thumbnail" src="../public/img/philips.png" alt=""></a></li>
-		</ul>	
-	</div>
-	<div class="search"><a href=""> Tra cứu thông tin bảo hành </a></div>
-	<div class="comment">
-	  <h3> <span> THẢO LUẬN BẢO HÀNH </span> </h3>
-	  <div class="clear-fix"> </div>
-	  <div class="content" style="width: 100%;"> 
-	  	<textarea name="" id="" cols="118" rows="5"> Nội dung </textarea>
-	  	<button type="button" class="btn btn-default pull-right"> Gửi bình luận </button>
-	  </div>
-	</div>
+    <h3>TRUNG TÂM BẢO HÀNH TRÊN TOÀN QUỐC</h3>
+    <link rel="stylesheet" href="../public/css/footer.css">
+    <link rel="stylesheet" href="../public/css/warranty.css">
+    <p><b>Thegioididong.com xin lỗi vì sự cố khiến điện thoại của quý khách bị hỏng và phải đi bảo hành. Thế Giới Di Động có 2 hỗ trợ dành riêng cho khách hàng mua điện thoại tại TGDĐ trong thời gian đi bảo hành như sau:
+    1. Nếu quý khách hàng muốn mượn máy để sử dụng trong thời gian gửi máy đi bảo hành, quý khách có thể mượn máy tại quầy máy đã sử dụng. Chi tiết giá trị máy mượn quý khách có thể hỏi nhân viên mượn máy hoặc xem trên biên nhận máy bảo hành.
+    2. Nếu máy gửi đi bảo hành quá 15 ngày (30 ngày đối với sản phẩm của Apple) hãng bảo hành chưa trả máy cho khách hàng, TGDĐ có thể hỗ trợ khách thu lại máy hỏng với mức giá hỗ trợ (đối với các sản phẩm iPhone sẽ áp dụng theo chính sách của Apple).
+    Lưu ý: Chỉ áp dụng thu mua cho điện thoại và phải còn trong điều kiện bảo hành.</b></p>
+    <p> <b>Chọn hãng</b> bảo hành: </p>
+    <div class="logo">
+        <ul style="list-style: none; width: 100%;padding: 10px 0;">
+            <li class="logo"><a href=""> <img class="thumbnail" src="../public/img/apple.png" alt=""></a></li>
+            <li class="logo"><a href=""> <img class="thumbnail" src="../public/img/Sony.png" alt=""></a></li>
+            <li class="logo"><a href=""> <img class="thumbnail" src="../public/img/Samsung.png" alt=""></a></li>
+            <li class="logo"><a href=""> <img class="thumbnail" src="../public/img/LG.png" alt=""></a></li>
+            <li class="logo"><a href=""> <img class="thumbnail" src="../public/img/nokia.png" alt=""></a></li>
+            <li class="logo"><a href=""> <img class="thumbnail" src="../public/img/oppo.png" alt=""></a></li>
+            <li class="logo"><a href=""> <img class="thumbnail" src="../public/img/philips.png" alt=""></a></li>
+        </ul>   
+    </div>
+    <div class="search"><a href=""> Tra cứu thông tin bảo hành </a></div>
+    <div class="comment">
+      <h3> <span> THẢO LUẬN BẢO HÀNH </span> </h3>
+      <div class="clear-fix"> </div>
+      <div class="content" style="width: 100%;"> 
+        <textarea name="" id="" cols="118" rows="5"> Nội dung </textarea>
+        <button type="button" class="btn btn-default pull-right"> Gửi bình luận </button>
+      </div>
+    </div>
 @stop
